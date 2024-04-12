9142bcf4c396a6082dde874f1511a0d46f7a3352
Charlotta Grundmann <lotta@grundmann.se>
2024-04-12 10:12:43 +0000

    Clean up code

--- pattern/models.py
+++ pattern/models.py
@@ -6,62 +6,62 @@ from django.utils.text import slugify
 
 LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
 
+
 class Pattern(models.Model):
-    pattern_name = models.CharField(max_length = 200, unique = True)
-    slug = models.SlugField(max_length = 200, unique = True)
-    description = models.TextField(default = "description")
-    created_by = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "patterncreator")
-    featured_image = CloudinaryField('image', default = 'https://res.cloudinary.com/dehwhmatn/image/upload/v1712673845/placeholder_pattern_y33fzt')
-    needle_size = models.CharField(max_length = 100)
-    gauge = models.CharField(max_length = 100)
-    yarn = models.CharField(max_length = 100, default = "yarn")
-    difficulty_level = models.IntegerField(choices = LEVEL,)
-    created_on = models.DateTimeField(auto_now_add = True)
-    likes = models.ManyToManyField(User, related_name = 'liked_patterns', blank = True)
+    pattern_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(default="description")
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, related_name="patterncreator")
+    featured_image = CloudinaryField('image')
+    needle_size = models.CharField(max_length=100)
+    gauge = models.CharField(max_length=100)
+    yarn = models.CharField(max_length=100, default="yarn")
+    difficulty_level = models.IntegerField(choices=LEVEL,)
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User,
+        related_name='liked_patterns', blank=True)
 
     class Meta:
         ordering = ["created_on"]
-
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.pattern_name)
         super(Pattern, self).save(*args, **kwargs)
 
-
     def __str__(self):
         return f"{self.pattern_name} | by {self.created_by}"
-
 
     def like(self, user):
         if user not in self.likes.all():
             self.likes.add(user)
 
-
-
     def unlike(self, user):
         if user in self.likes.all():
             self.likes.remove(user)
-
 
     def like_count(self):
         return self.likes.count()
 
 
 class Comment(models.Model):
-    pattern = models.ForeignKey(Pattern, on_delete = models.CASCADE, related_name = "comments")
+    pattern = models.ForeignKey(
+        Pattern, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete = models.CASCADE, related_name = "commenter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add = True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["created_on"]
-
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
 
 
 class ProjectList(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    patterns = models.ManyToManyField(Pattern, related_name = 'project_lists')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    patterns = models.ManyToManyField(Pattern, related_name='project_lists')
+    
--- pattern/views.py
+++ pattern/views.py
@@ -5,7 +5,8 @@ from django.views.decorators.http import require_POST
 from django.views import generic
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView, View
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseBadRequest
 from django.forms import ModelForm
 from django.utils.text import slugify
 from django.http import JsonResponse
@@ -14,22 +15,21 @@ from .forms import CommentForm, PatternForm
 import cloudinary.uploader
 
 
-
 class PatternList(generic.ListView):
     model = Pattern
     template_name = "pattern/patterns.html"
     paginate_by = 6
 
 
-#@login_required
+@login_required
 def pattern_details(request, slug):
-    pattern = get_object_or_404(Pattern, slug = slug)
-    comments = Comment.objects.filter(pattern = pattern).order_by("-created_on")
+    pattern = get_object_or_404(Pattern, slug=slug)
+    comments = Comment.objects.filter(pattern=pattern).order_by("-created_on")
 
     if request.method == "POST":
-        comment_form = CommentForm(data = request.POST)
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            comment = comment_form.save(commit = False)
+            comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.pattern = pattern
             comment.save()
@@ -37,7 +37,7 @@ def pattern_details(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted'
             )
-    
+
     comment_form = CommentForm()
 
     # Count the number of likes for the pattern
@@ -60,9 +60,9 @@ def comment_edit(request, slug, comment_id):
 
     if request.method == "POST":
 
-        post = get_object_or_404(Pattern, slug = slug)
-        comment = get_object_or_404(Comment, pk = comment_id)
-        comment_form = CommentForm(data = request.POST, instance = comment)
+        post = get_object_or_404(Pattern, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
@@ -73,13 +73,13 @@ def comment_edit(request, slug, comment_id):
             messages.add_message(request, messages.ERROR,
                                  'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('pattern_details', args = [slug]))
+    return HttpResponseRedirect(reverse('pattern_details', args=[slug]))
 
 
 @login_required
 def comment_delete(request, slug, comment_id):
-    post = get_object_or_404(Pattern, slug = slug)
-    comment = get_object_or_404(Comment, pk = comment_id)
+    post = get_object_or_404(Pattern, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
         comment.delete()
@@ -88,7 +88,7 @@ def comment_delete(request, slug, comment_id):
         messages.add_message(request, messages.ERROR,
                              'You can only delete your own comments!')
 
-    return HttpResponseRedirect(reverse('pattern_details', args = [slug]))
+    return HttpResponseRedirect(reverse('pattern_details', args=[slug]))
 
 
 @login_required
@@ -96,29 +96,31 @@ def add_pattern(request):
     if request.method == "POST":
         pattern_form = PatternForm(request.POST, request.FILES)
         if pattern_form.is_valid():
-            pattern = pattern_form.save(commit = False)
+            pattern = pattern_form.save(commit=False)
             pattern.created_by = request.user
             pattern.slug = slugify(pattern.pattern_name)
             pattern.save()
             messages.success(request, 'Your pattern was added successfully!')
             return redirect('patterns')
-        
+
     else:
         pattern_form = PatternForm()
-    
-    return render(request, "pattern/add_pattern.html", {'pattern_form': pattern_form})
+
+    return render(request, "pattern/add_pattern.html", 
+        {'pattern_form': pattern_form
+        })
 
 
 @login_required
 def like_pattern(request, slug):
-    pattern = get_object_or_404(Pattern, slug = slug)
+    pattern = get_object_or_404(Pattern, slug=slug)
 
     if pattern.created_by == request.user:
         messages.add_message(
             request, messages.ERROR, "You can't like your own pattern!")
     else:
 
-        if pattern.likes.filter(id = request.user.id).exists():
+        if pattern.likes.filter(id=request.user.id).exists():
             pattern.likes.remove(request.user)
             messages.add_message(
             request, messages.ERROR, "Didn't like the pattern? Well, there are other nice patterns!")
@@ -127,7 +129,7 @@ def like_pattern(request, slug):
             messages.add_message(
                 request, messages.SUCCESS, 'We too like this pattern!')
 
-    return HttpResponseRedirect(reverse('pattern_details', args = [slug]))
+    return HttpResponseRedirect(reverse('pattern_details', args=[slug]))
 
 
 @login_required
@@ -135,7 +137,7 @@ def add_to_project_list(request, slug):
     pattern = get_object_or_404(Pattern, slug=slug)
     user = request.user
     project_list, _ = ProjectList.objects.get_or_create(user=user)
-    
+
     if pattern in project_list.patterns.all():
         messages.error(request, 'Pattern is already in your project list.')
     else:
@@ -161,19 +163,19 @@ def my_page(request):
 
 
 def get_liked_pattern_names(user):
-    liked_patterns = Pattern.objects.filter(likes = user)
+    liked_patterns = Pattern.objects.filter(likes=user)
     pattern_names_and_slugs = [(pattern.pattern_name, pattern.slug) for pattern in liked_patterns]
     return pattern_names_and_slugs
 
 
 def get_pattern_created_by(user):
-    my_patterns = Pattern.objects.filter(created_by = user)
+    my_patterns = Pattern.objects.filter(created_by=user)
     pattern_names_and_slugs = [(pattern.pattern_name, pattern.slug) for pattern in my_patterns]
     return pattern_names_and_slugs
 
 
 def get_added_to_list(user):
-    project_list = ProjectList.objects.filter(user = user).first()
+    project_list = ProjectList.objects.filter(user=user).first()
     if project_list:
         pattern_names_and_slugs = [(pattern.pattern_name, pattern.slug) for pattern in project_list.patterns.all()]
         return pattern_names_and_slugs
